0ff1d0fa66f27197da041d31f6a62d961937fa32
at2224 <at2224@njit.edu>
2024-10-26 19:58:51 -0400

    Can now open a window containing a message using electron node.js

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
+// require app and browser window from electron
 const {app, BrowserWindow} = require('electron');
 
+// window specs
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    // target
+    win.loadFile('src/index.html');
+}
+
+// create window
+app.whenReady().then(createWindow);
+
+// closes electron when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') app.quit();
+})
